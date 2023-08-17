793d581449ed8dc64ea211ed53800f51f52343b6
RuthalaKiran <125887544+RuthalaKiran@users.noreply.github.com>
2023-08-17 23:39:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,6 @@ newquote(api);
 function twiter(){
   window.open("https://twitter.com/intent/tweet?text=" +quotebody.innerHTML)
 }
+function copy(){
+  navigator.clipboard.writeText(quotebody.innerHTML);
+}
