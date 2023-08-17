69795edb3e70d46d1d770625bf07c48a15e4460c
RuthalaKiran <125887544+RuthalaKiran@users.noreply.github.com>
2023-08-17 23:03:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const quotebody = document.querySelector(".quote-body");
+const quoteauther = document.querySelector(".quote-author");
+
+const api = "https://api.quotable.io/random";
+async function  newquote(url){
+  const response = await fetch(url);
+  var data = await response.json();
+  console.log(data);
+  quotebody.innerHTML = data.content;
+  quoteauther.innerHTML = ` 𑁋 ${data.author}`;
+}
+newquote(api);
+
+function twiter(){
+  window.open("https://twitter.com/intent/tweet?text=" +quotebody.innerHTML)
+}
